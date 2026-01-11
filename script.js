df7b204b20480a5449431f2ce61b964bd2b63a44
Ru83n04 <rubimonfils13@gmail.com>
2026-01-11 13:45:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const symbols = ["♠", "♥", "♦", "♣"];
 
+function drawCard() {
+  const symbol = symbols[Math.floor(Math.random() * symbols.length)];
+  document.getElementById("status").innerText =
+    `Gezogene Karte: ${symbol} – Symbol-Spieler trinkt 🍺`;
+}
